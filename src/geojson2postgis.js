0ed2ac9a2b3c5f20b6b7666b769cccadfded6a1f
Thomas Gratier <thomas_gratier@yahoo.fr>
2019-07-08 13:43:37 +0200

    Change to manage name in GeoJSON properties

--- src/geojson2postgis.js
+++ src/geojson2postgis.js
@@ -2,12 +2,14 @@ function geojson2postgis(db, tableName, geojson) {
   const features = geojson.features.map(function getRow(feature) {
     return {
       geom: db.raw(`st_setsrid(st_geomfromgeojson('${JSON.stringify(feature.geometry)}'), 4326)`),
-      id: feature.id
+      id: feature.id,
+      name: feature.properties.name || `Trajectoire ${feature.id}`
     };
   });
 
   return db.schema.createTableIfNotExists(tableName, function (table) {
-    table.integer('id').defaultTo(null);
+    table.integer('id');
+    table.string('name');
     table.specificType('geom', 'geometry(GEOMETRY, 4326)').notNullable();
   }).then(function () {
     return db(tableName).insert(features);
@@ -15,4 +17,3 @@ function geojson2postgis(db, tableName, geojson) {
 }
 
 module.exports = geojson2postgis;
-
